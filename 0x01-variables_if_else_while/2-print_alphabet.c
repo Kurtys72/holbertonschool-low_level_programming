bbe6413de107a9d6e27e4c5e3a57b27c7522318b
Kurtys72 <kurtys.djamai@gmail.com>
2021-10-02 08:18:38 +0200

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,25 +1,18 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints the alphabetic
  *
- * Return: Always 0 (Success)
+ * Return: Always (Success)
  */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
-		putchar('\n');
 
+	putchar('\n');
 	return (0);
 }
 

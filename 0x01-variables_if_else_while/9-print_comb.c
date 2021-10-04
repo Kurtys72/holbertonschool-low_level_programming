a1973d794fc3c7d730273aa74dd4159a1e1b5fea
Kurtys72 <kurtys.djamai@gmail.com>
2021-10-04 13:45:41 +0200

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - Prints a hexadecimal string
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	int c;
+
+	for (c = '48'; c <= '57'; c++)
+	{
+		putchar(c);
+		if (c != 57)
+	{        putchar (',');
+	  putchar (' ');
+	}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}

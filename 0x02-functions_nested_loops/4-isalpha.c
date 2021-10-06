204761be1a62d6fbdd0f27ba59ccbca879ace892
Kurtys72 <kurtys.djamai@gmail.com>
2021-10-06 13:40:53 +0200

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,20 @@
-#include "main.h"
+#include <ctype.h>
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * _isaalpha - check character
+ * @c: integer value
+ * Return: always succes(0)
  */
-char c()
+int _isalpha(int c)
+{
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+

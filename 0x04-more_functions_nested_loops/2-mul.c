519effab5112a9e21b725dfb4cf65f2214b08075
Kurtys72 <kurtys.djamai@gmail.com>
2021-10-07 12:07:08 +0200

    *

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * int mul - check character
+ * @a&b: integer value
+ * Return: always succes(0)
+ */
+int mul(int a, int b)
+	
+{
+	return (a * b);
+}

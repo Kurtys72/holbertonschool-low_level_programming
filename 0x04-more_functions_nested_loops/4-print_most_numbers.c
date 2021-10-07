ba16835abf358c60d9bc82a8d41073338c01b4eb
Kurtys72 <kurtys.djamai@gmail.com>
2021-10-07 15:00:57 +0200

    *

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,14 +4,13 @@
 * print_numbers- printing
 * Return: always succes(0)
 */
-void print_numbers(void)
+void print_most_numbers(void)
 {
-	{
 	int z;
-
 	for (z = 0; z <= 9; z++)
 		if (z != 2 && z != 4)
-			_putchar(z + '0');
-	}
-	_putchar('\n');
+		{
+			_putchar (z + '0');
+		}
+	_putchar ('\n');
 }

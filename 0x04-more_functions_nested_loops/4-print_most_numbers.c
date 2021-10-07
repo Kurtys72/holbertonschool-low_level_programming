5308c739fc22f100bb7e17bcc43d74c9903acb1b
Kurtys72 <kurtys.djamai@gmail.com>
2021-10-07 15:04:37 +0200

    *

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
-* print_numbers- printing
+* print_most_numbers- printing
 * Return: always succes(0)
 */
 void print_most_numbers(void)
 {
 	int z;
+
 	for (z = 0; z <= 9; z++)
 		if (z != 2 && z != 4)
 		{
